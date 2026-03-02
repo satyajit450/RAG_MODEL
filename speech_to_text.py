16524227d2cd3afe7b321b080cb7bd238df9da55
satyajit450 <satyajeetsahu53@gmail.com>
2026-03-02 23:54:23 +0530

    Adding faster whisper

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,3 +1,12 @@
-import whisper
+from faster_whisper import WhisperModel
 
-model = whisper.load_model("medium")
+model = WhisperModel(
+    "tiny",        # tiny is safest for your laptop
+    device="cpu",
+    compute_type="int8"   # VERY IMPORTANT → reduces RAM usage
+)
+
+segments, info = model.transcribe("audios/videoplayback_5.mp3")
+
+for segment in segments:
+    print(segment.text)
