541eebca39bd46d52e026685d412c29939bf7024
satyajit450 <satyajeetsahu53@gmail.com>
2026-03-03 21:49:53 +0530

    Creating chunks

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,12 +1,23 @@
 from faster_whisper import WhisperModel
+import json
 
 model = WhisperModel(
-    "tiny",        # tiny is safest for your laptop
+    "tiny",        
     device="cpu",
     compute_type="int8"   # VERY IMPORTANT → reduces RAM usage
 )
 
 segments, info = model.transcribe("audios/videoplayback_5.mp3")
 
+chunks =[] 
+
 for segment in segments:
-    print(segment.text)
+    chunks.append({
+        "Start": segment.start,   # dot notation
+        "End": segment.end,       # dot notation
+        "Text": segment.text      # dot notation
+    })
+for i in range(5):
+    with open(f"chunks/video_{i+1}.json", "w") as f :
+        json.dump(chunks , f)
+
