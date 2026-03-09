6062798f0560aae98d475d62c98ad9d77bdfd0ea
satyajit450 <satyajeetsahu53@gmail.com>
2026-03-10 00:31:55 +0530

    Adding complete embeddibg file

--- embedding.py
+++ embedding.py
@@ -1,9 +1,34 @@
 from ollama import embed
-def embedding(text):
+import os, json
+import pandas as pd
+
+def create_embedding(text):
     response = embed(
         model='bge-m3',
         input=text
     )
     return response
 
-print(embedding("Hello, world!")["embeddings"][0][:10])
+jsons = os.listdir("chunks")
+my_dict = []
+chunk_id = 0
+
+for json_file in jsons:
+    with open(f"chunks/{json_file}") as f:
+        content = json.load(f)
+
+    for chunk in content['chunks']:  # simple loop, no enumerate needed
+        chunk['chunk_id'] = chunk_id
+        chunk_id += 1
+
+        # create embedding per chunk
+        emb = create_embedding(chunk["Text"])["embeddings"][0]
+        chunk["embedding"] = emb
+
+        my_dict.append(chunk)
+
+# create dataframe
+df = pd.DataFrame.from_records(my_dict)
+
+# print only summary, not full vectors
+print(df)
