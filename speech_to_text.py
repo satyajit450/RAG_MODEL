f5e956361d8128de94bd46ecd2e1874f8583dbe3
satyajit450 <satyajeetsahu53@gmail.com>
2026-03-19 23:56:52 +0530

    update file

--- speech_to_text.py
+++ speech_to_text.py
@@ -15,22 +15,21 @@ for audio in audio_files:
         if audio.endswith(".mp3"):
             segments, info = model.transcribe(f"audios/{audio}")
 
-        chunks = []
-        
-        for segment in segments:
-            chunks.append({
-                "title": segment.title,
-                "Start": segment.start,
-                "End": segment.end,
-                "Text": segment.text
-            })
+            chunks = []
+            
+            for segment in segments:
+                chunks.append({
+                    "Start": segment.start,
+                    "End": segment.end,
+                    "Text": segment.text
+                })
 
-        chunks_with_metadata = {
-            "chunks": chunks,
-            "full_text": " ".join([c["Text"] for c in chunks])
-        }
+            chunks_with_metadata = {
+                "chunks": chunks,
+                "full_text": " ".join([c["Text"] for c in chunks])
+            }
 
-        output_name = os.path.splitext(audio)[0] + ".json"
+            output_name = os.path.splitext(audio)[0] + ".json"
 
-        with open(f"chunks/{output_name}", "w") as f:
-            json.dump(chunks_with_metadata, f, indent=4)
+            with open(f"chunks/{output_name}", "w") as f:
+                json.dump(chunks_with_metadata, f, indent=4)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,19 @@
-pandas
+     chunks = []
+            
+            for segment in segments:
+                chunks.append({
+                    "title": segment.title,
+                    "Start": segment.start,
+                    "End": segment.end,
+                    "Text": segment.text
+                })
+
+            chunks_with_metadata = {
+                "chunks": chunks,
+                "full_text": " ".join([c["Text"] for c in chunks])
+            }
+
+            output_name = os.path.splitext(audio)[0] + ".json"
+
+            with open(f"chunks/{output_name}", "w") as f:
+                json.dump(chunks_with_metadata, f, indent=4)
